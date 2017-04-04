a00086329ff136b4bc9f86a81861c6196f9fc7ae
Hendrik <asdasd@xn--sd12-poa.de>
2017-04-05 00:15:13 +0200

    Add Logging

--- src/main/java/de/inovex/example/controller/TodoController.java
+++ src/main/java/de/inovex/example/controller/TodoController.java
@@ -2,6 +2,8 @@ package de.inovex.example.controller;
 
 import de.inovex.example.model.Todo;
 import de.inovex.example.model.TodoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,21 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TodoController {
 
+    Logger logger = LoggerFactory.getLogger(TodoController.class);
+
     @Autowired
     TodoRepository todoRepository;
 
     @RequestMapping("/todo/{todoId}")
     Todo getTodo(@PathVariable("todoId") Long id){
+        logger.info("Accessed single todo {}",id);
         return todoRepository.findOne(id);
     }
 
     @RequestMapping("/todo")
     Iterable<Todo> getAll(){
+        logger.info("Accessed all todos");
         return todoRepository.findAll();
     }
 
     @RequestMapping("/todo/undone")
     Iterable<Todo> getAllUndone(){
+        logger.info("Accessed all undone todos");
         return todoRepository.findByDone(false);
     }
 
